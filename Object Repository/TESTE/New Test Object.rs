cdc22fb1b9a78534a839249fd79389993cfe751c
cdmeirelles <cdmeirelles@note-poa008.dm_tools.local>
2019-08-12 10:10:12 -0300

    TESTE

---
+++ Object Repository/TESTE/New Test Object.rs
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>New Test Object</name>
+   <tag></tag>
+   <elementGuidId>e20c6d94-9613-4c32-b650-cf91e206d8b7</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
