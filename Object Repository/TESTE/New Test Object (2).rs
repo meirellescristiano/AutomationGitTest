579d3501eb04fd7b97428729390c0c9dce8d5e94
cdmeirelles <cdmeirelles@note-poa008.dm_tools.local>
2019-08-12 10:17:38 -0300

    Teste

---
+++ Object Repository/TESTE/New Test Object (2).rs
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>New Test Object (2)</name>
+   <tag></tag>
+   <elementGuidId>373570e0-84bb-4042-8700-a2655dc73d64</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
