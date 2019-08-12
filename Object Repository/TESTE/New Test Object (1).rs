f218e6b24ba779dfaae5c43bc603ec96a548d3a5
cdmeirelles <cdmeirelles@note-poa008.dm_tools.local>
2019-08-12 10:18:56 -0300

    TESTE

--- Object Repository/TESTE/New Test Object (1).rs
+++
@@ -1,9 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<WebElementEntity>
-   <description></description>
-   <name>New Test Object (1)</name>
-   <tag></tag>
-   <elementGuidId>976e579c-8480-4cb6-8e72-ca4c4d11e010</elementGuidId>
-   <selectorMethod>BASIC</selectorMethod>
-   <useRalativeImagePath>false</useRalativeImagePath>
-</WebElementEntity>
--- Object Repository/TESTE/New Test Object.rs
+++
@@ -1,9 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<WebElementEntity>
-   <description></description>
-   <name>New Test Object</name>
-   <tag></tag>
-   <elementGuidId>e20c6d94-9613-4c32-b650-cf91e206d8b7</elementGuidId>
-   <selectorMethod>BASIC</selectorMethod>
-   <useRalativeImagePath>false</useRalativeImagePath>
-</WebElementEntity>
